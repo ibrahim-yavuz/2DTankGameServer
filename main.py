8a83f3e988e35feccf6149770a2140c011a71ea2
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-05-11 02:38:24 +0300

    reformats code

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+from http import server
 import config
 from socket_operations import *
 
-get_connected_clients()
-
-while True:
-    try:
-        player_info = get_player_info()
-        send_player_info(player_info)
-    except:
-        print("An error has occured")
+server_socket = SocketOperations(config.host['port'])
+print(server_socket.get_connected_clients())
+server_socket.start_the_game()
--- socket_operations.py
+++ socket_operations.py
@@ -6,53 +6,64 @@ from player_info import PlayerInfo
 
 buffer_size = config.host['buffer_size']
 host_ip = config.host['ip']
-port = config.host['port']
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((host_ip, port))
+class SocketOperations:
+    players = []
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-players = []
+    def __init__(self, port):
+        self.UDPServerSocket.bind((host_ip, port))
 
-def send_command(command):
-    bytesToSend = str.encode(command)
-    for player in players:
-        UDPServerSocket.sendto(bytesToSend, player.address)
+    def send_command(self, command):
+        bytesToSend = str.encode(command)
+        for player in self.players:
+            self.UDPServerSocket.sendto(bytesToSend, player.address)
         
-def get_connected_clients() -> str:
-    while True:
-        bytesAddressPair = UDPServerSocket.recvfrom(buffer_size)
+    def get_connected_clients(self) -> str:
+        while True:
+            bytesAddressPair = SocketOperations.UDPServerSocket.recvfrom(buffer_size)
+            message = bytesAddressPair[0].decode('utf-8')
+            address = bytesAddressPair[1]
+
+            if message != config.commands['start']:
+                player = Player(message, address)
+                if player not in self.players:
+                    self.players.append(player)
+                self.send_player_data()
+            else:
+                self.send_command(command=message)
+                break
+        return self.players
+
+
+    def send_player_data(self):
+        new_json = "["
+        for player in self.players:
+            if player == self.players[len(self.players) - 1]:
+                new_json = new_json + player.toJson()
+            else:
+                new_json = new_json + player.toJson() + ","
+        new_json = json.loads(new_json + "]")
+        self.send_command(json.dumps(new_json))
+        return new_json
+
+    def send_player_info(self, playerInfo):
+        bytesToSend = str.encode(playerInfo.toJson())
+        for player in self.players:
+            self.UDPServerSocket.sendto(bytesToSend, player.address)
+
+
+    def get_player_info(self) -> PlayerInfo:
+        bytesAddressPair = self.UDPServerSocket.recvfrom(buffer_size)
         message = bytesAddressPair[0].decode('utf-8')
-        address = bytesAddressPair[1]
+        playerInfo = PlayerInfo.fromJson(message)
+        return playerInfo
 
-        if message != config.commands['start']:
-            player = Player(message, address)
-            if player not in players:
-                players.append(player)
-            print(send_player_data())
-        else:
-            send_command(command=message)
-            break
+    def start_the_game(self):
+        while True:
+            try:
+                player_info = self.get_player_info()
+                self.send_player_info(player_info)
+            except:
+                print("An error has occured")
 
-
-def send_player_data():
-    new_json = "["
-    for player in players:
-        if player == players[len(players) - 1]:
-            new_json = new_json + player.toJson()
-        else:
-            new_json = new_json + player.toJson() + ","
-    new_json = json.loads(new_json + "]")
-    send_command(json.dumps(new_json))
-    return new_json
-
-def send_player_info(playerInfo):
-    bytesToSend = str.encode(playerInfo.toJson())
-    for player in players:
-        UDPServerSocket.sendto(bytesToSend, player.address)
-
-
-def get_player_info() -> PlayerInfo:
-    bytesAddressPair = UDPServerSocket.recvfrom(buffer_size)
-    message = bytesAddressPair[0].decode('utf-8')
-    playerInfo = PlayerInfo.fromJson(message)
-    return playerInfo
