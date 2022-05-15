181a33203c506fd7b1ad491970cd1f3f9f988c5e
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-05-15 18:31:37 +0300

    removed get_connected_clients code line from main.py

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from socket_operations import *
 
 main_server_socket = SocketOperations(5555)
 print("Started the server at - {}:{}".format(main_server_socket.get_host(), main_server_socket.get_port_number()))
-main_server_socket.get_connected_clients()
 main_server_socket.start_the_game()
