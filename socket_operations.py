beb3c910b4fb132a0fbd299435901935ad1972b1
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-05-15 19:39:40 +0300

    bug fix

--- socket_operations.py
+++ socket_operations.py
@@ -28,19 +28,21 @@ class SocketOperations:
     def send_data(self, data):
         data = str.encode(data)
         for player in self.players:
+            print(player.address)
             self.UDPServerSocket.sendto(data, player.address)
 
 
     def get_data(self):
         bytesAddressPair = self.UDPServerSocket.recvfrom(buffer_size)
         message = bytesAddressPair[0].decode('utf-8')
+        address = bytesAddressPair[1]
 
         print(message)
 
         data = Data.fromJson(message)
 
         if(data.dataType == "PlayerConnected"):
-            player_data = Player.fromJson(json.dumps(data.incomingData))
+            player_data = Player(data.incomingData.username, address)
             
             if(player_data not in self.players):
                 print(player_data.toJson())
